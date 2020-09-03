800ce154b3924fba6bb588ff85176b35d931e2d5
Mukhammad Imam Nur Hidayat <dayat@mhs.stmik-yadika.ac.id>
2020-09-04 02:11:21 +0700

    delete data json from local server

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,11 +1,28 @@
 import React, {Component, Fragment} from 'react';
 import './BlogPost.css';
-import Post from '../component/Post/Post';
+import Post from '../../component/Post/Post';
 import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
         post: []
+    }
+
+    getPostAPI = () => {
+        axios.get('http://localhost:3004/posts')
+        .then((result) => {
+            console.log(result.data)
+            this.setState({
+                post: result.data
+            })
+        })
+    }
+
+    handleRemove = (data) => {
+        console.log(data);
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+            this.getPostAPI();
+        })
     }
 
     componentDidMount(){
@@ -16,13 +33,7 @@ class BlogPost extends Component {
         //         post: json
         //     })
         // })
-        axios.get('http://localhost:3004/posts')
-        .then((result) => {
-            console.log(result.data)
-            this.setState({
-                post: result.data
-            })
-        })
+        this.getPostAPI();
     }
     render(){
         return(
@@ -30,7 +41,7 @@ class BlogPost extends Component {
                 <p className="section-title">Blog Post</p>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} title={post.title} desc={post.body} />
+                        return <Post key={post.id} data={post} remove={this.handleRemove} />
                     })
                 }                
             </Fragment>
