0afd36607b703f2a0ea71916cc2c2505a069094c
Mukhammad Imam Nur Hidayat <dayat@mhs.stmik-yadika.ac.id>
2020-09-05 00:27:42 +0700

    handle form change event and set state

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,13 +5,18 @@ import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
-        post: []
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        }
     }
 
     getPostAPI = () => {
         axios.get('http://localhost:3004/posts')
         .then((result) => {
-            console.log(result.data)
             this.setState({
                 post: result.data
             })
@@ -22,6 +27,17 @@ class BlogPost extends Component {
         console.log(data);
         axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
             this.getPostAPI();
+        })
+    }
+
+    handleFormChange = (event) => {
+        let formBlogPostNew = {...this.state.formBlogPost};
+        console.log(event.target.name);
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        }, () => {
+            console.log('value object formBlogPost', this.state.formBlogPost)
         })
     }
 
@@ -39,6 +55,13 @@ class BlogPost extends Component {
         return(
             <Fragment>
                 <p className="section-title">Blog Post</p>
+                <div className="form-add-post">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange} />
+                    <label htmlFor="body">Blog Content</label>
+                    <textarea type="text" name="body" id="body" col="30" row="10" placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+                    <button className="btn-submit">Simpan</button>
+                </div>
                 {
                     this.state.post.map(post => {
                         return <Post key={post.id} data={post} remove={this.handleRemove} />
